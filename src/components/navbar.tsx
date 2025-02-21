77e283046da0bcb2876a6279c79a88e257e4626c
Victor Gabriel <88253070+VictorBritto@users.noreply.github.com>
2025-02-21 09:02:15 -0300

    primeiro commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,16 @@
+import '../styles/navbar.css';
+
+export function Navbar(){
+    return(
+    <nav>
+        <div>
+            <h1 id='title'>Victor Gabriel</h1>
+            <ul>
+                <li><a href="#" id='text1'>Projetos</a></li>
+                <li><a href="#" id='text1'>Sobre</a></li>
+                <li><a href="#" id='text1'>Currículo</a></li>
+            </ul>
+        </div>
+    </nav>
+    )
+}
